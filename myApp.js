a32a85fe67364676ac887fb1f80831d2fc4e751c
Jenssen Lee <jenssen.lee@gmail.com>
2025-03-17 09:44:31 +0000

    Add mongoose connect

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
+let mongoose;
 require('dotenv').config();
+mongoose = require('mongoose');
 
-
-let Person;
+try {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+} catch (error) {
+  console.error('error', error)
+}
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
