922f8cf82b9fbce684faf3d6b7c63c0225349c61
Jenssen Lee <jenssen.lee@gmail.com>
2025-03-21 09:23:57 +0000

    Update createAndSavePerson function

--- myApp.js
+++ myApp.js
@@ -16,7 +16,12 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const Jenssen = new Person({ name: "Jenssen", age: 29, favoriteFoods: ["banana", "ramen"] })
+
+  Jenssen.save((error, data) => {
+    if (error) return console.error(error);
+    done(null, data);
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
@@ -69,14 +74,14 @@ const queryChain = (done) => {
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
-// exports.PersonModel = Person;
-// exports.createAndSavePerson = createAndSavePerson;
-// exports.findPeopleByName = findPeopleByName;
-// exports.findOneByFood = findOneByFood;
-// exports.findPersonById = findPersonById;
-// exports.findEditThenSave = findEditThenSave;
-// exports.findAndUpdate = findAndUpdate;
-// exports.createManyPeople = createManyPeople;
-// exports.removeById = removeById;
-// exports.removeManyPeople = removeManyPeople;
-// exports.queryChain = queryChain;
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
